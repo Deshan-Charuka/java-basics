0d6c7b2e3fd4c4dc01870840eaf9a9eee6ded48f
Deshan Charuka <d.c.0729439631@gmail.com>
2022-10-22 03:04:05 +0530

    Add getters setters to the Employee class

--- src/com/charuka/deshan/Employee.java
+++ src/com/charuka/deshan/Employee.java
@@ -5,9 +5,28 @@ package com.charuka.deshan;
  * @since : 2022-10-22
  **/
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
+    public void setBaseSalary(int baseSalary){
+        if (baseSalary < 0)
+            throw new IllegalArgumentException("Salary should be larger than zero!!");
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary(){
+        return baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate){
+        if (hourlyRate < 0)
+            throw new IllegalArgumentException("Hourly Rate should be larger than zero!!");
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHourlyRate(){
+        return hourlyRate;
+    }
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
     }
