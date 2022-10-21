553a271cf2491a658da54a6a01f15917daf2fcd0
Deshan Charuka <d.c.0729439631@gmail.com>
2022-10-21 20:52:21 +0530

    Create TextBox class

--- src/com/charuka/deshan/TextBox.java
+++ src/com/charuka/deshan/TextBox.java
@@ -5,5 +5,13 @@ package com.charuka.deshan;
  * @since : 2022-10-21
  **/
 public class TextBox {
+    public String text;
 
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void clear() {
+        text = "";
+    }
 }
