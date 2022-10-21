3a39cd8ec32651d9194f1c8d84e4bbbba796eb82
Deshan Charuka <d.c.0729439631@gmail.com>
2022-10-21 20:47:21 +0530

    Initial commit

---
+++ src/com/charuka/deshan/Main.java
@@ -0,0 +1,11 @@
+package com.charuka.deshan;
+
+/**
+ * @author : Deshan Charuka <d.c.0729439631@gmail.com>
+ * @since : ${DATE}
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
