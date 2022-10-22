e4fc770cc373c0ed4d94a01a85899564a5d0fce5
Deshan Charuka <d.c.0729439631@gmail.com>
2022-10-22 13:01:03 +0530

    Reduce coupling with Employee class by privating unwanted methods

--- src/com/charuka/deshan/Employee.java
+++ src/com/charuka/deshan/Employee.java
@@ -8,25 +8,24 @@ public class Employee {
     private int baseSalary;
     private int hourlyRate;
 
-    public void setBaseSalary(int baseSalary){
-        if (baseSalary < 0)
-            throw new IllegalArgumentException("Salary should be larger than zero!!");
-        this.baseSalary = baseSalary;
-    }
-
-    public int getBaseSalary(){
+    private int getBaseSalary() {
         return baseSalary;
     }
 
-    public void setHourlyRate(int hourlyRate){
-        if (hourlyRate < 0)
-            throw new IllegalArgumentException("Hourly Rate should be larger than zero!!");
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary < 0) throw new IllegalArgumentException("Salary should be larger than zero!!");
+        this.baseSalary = baseSalary;
+    }
+
+    private int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate < 0) throw new IllegalArgumentException("Hourly Rate should be larger than zero!!");
         this.hourlyRate = hourlyRate;
     }
 
-    public int getHourlyRate(){
-        return hourlyRate;
-    }
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
     }
