2f478a33e2c23f451c502104db3cdb8218d1a898
Deshan Charuka <d.c.0729439631@gmail.com>
2022-10-22 20:27:12 +0530

    Overload employee constructor

--- src/com/charuka/deshan/Main.java
+++ src/com/charuka/deshan/Main.java
@@ -6,6 +6,7 @@ package com.charuka.deshan;
  **/
 public class Main {
     public static void main(String[] args) {
+        var employee1 = new Employee(50_000);
         var employee = new Employee(50_000, 20);
 //        int wage = employee.calculateWage(10);
         int wage = employee.calculateWage();
